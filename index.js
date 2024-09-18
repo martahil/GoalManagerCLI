8e24b31c0fa6f5c131489eff530d202b564217b1
Marta Hilgert <marta.hil.1988@gmail.com>
2024-09-18 13:01:38 -0300

    App menu (add, list & go back)

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const start = () => {
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
 
   while(true) {
-    let option = 'create goal'
+
+    const option = await select({
+      message: 'Menu >',
+      choices: [
+        {
+          name: 'Create goal',
+          value: 'add'
+        },
+        {
+          name: 'List goals',
+          value: 'list'
+        },
+        {
+          name: "Go back",
+          value: 'go back'
+        }
+      ]        
+    })
+
     switch(option) {
-      case 'create goal':
-        console.log("let's add")
+      case 'add':
+        console.log("Let's add")
         break
       case 'list':
-        console.log("let's list")
+        console.log("Let's list")
         break
       case 'go back':
+        console.log("See you soon")
         return
     }
   }
